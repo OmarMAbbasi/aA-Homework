61da9904d2faf940c0fee841a402e8af7e602783
Omar Abbasi <omarmabbasi@gmail.com>
2019-05-16 21:28:40 -0700

    Fixed all methods to use instance variable instead of local variable

--- W1D5/queue.rb
+++ W1D5/queue.rb
@@ -4,15 +4,15 @@ class Queue
     end
 
     def enqueue(el)
-        queue += [el]    
+        @queue += [el]    
     end
     
     def dequeue
-        queue.delete_at(0)    
+        @queue.delete_at(0)    
     end
     
     def peek
-        return queue[0]
+        return @queue[0]
     end
 end
 
