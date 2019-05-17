9b32ff90bd953627fab7d84893876350a2962512
Omar Abbasi <omarmabbasi@gmail.com>
2019-05-16 21:27:28 -0700

    Wrote test cases for Class Queue

--- W1D5/queue.rb
+++ W1D5/queue.rb
@@ -15,3 +15,14 @@ class Queue
         return queue[0]
     end
 end
+
+
+  test_queue = Queue.new
+  puts "Push 1: #{test_queue.enqueue(1)}"
+  puts "Pop: #{test_queue.dequeue}"
+  puts "Push 1: #{test_queue.enqueue(1)}"
+  puts "Push 2: #{test_queue.enqueue(2)}"
+  puts "Push 3: #{test_queue.enqueue(3)}"
+  puts "Peek: #{test_queue.peek}"
+  puts "Pop: #{test_queue.dequeue}"
+  puts "Peek: #{test_queue.peek}"
