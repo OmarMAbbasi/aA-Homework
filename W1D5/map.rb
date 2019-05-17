27254bd6b9b5811268fb0fb83d8b663e06bab9ea
Omar Abbasi <omarmabbasi@gmail.com>
2019-05-17 00:30:13 -0700

    Added a method delete that takes a key and returns false if that key doesn't exist, else it deletes the key/value from the hash.

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -20,13 +20,19 @@ class Map
         else
             @map[idx][1]
         end
-
     end
     
     def delete(key)
-
+        idx = has_key?(@map, key)
+        if !idx
+            return false
+        else
+            #debugger
+            @map[idx] = nil
+            @map.compact
+        end
     end
-    
+
     def show
 
     end
@@ -35,11 +41,6 @@ class Map
         to_check.each.with_index{|h, idx| return idx if h[0] == key } #return index of key/value pair if key exists, else return false
         return false
     end
-    
-    def inspect
-        p self
-    end
-    
 
 end
 
@@ -49,3 +50,5 @@ p "update value: #{test_map.set(1,3)}"
 p "add second value: #{test_map.set(2,4)}"
 p "return false if key doesn't exist: #{test_map.get(3)}"
 p "return value if key exists: #{test_map.get(2)}"
+puts "return false if key doesn't exist: #{test_map.delete(5)}"
+puts "delete value if key exists: #{test_map.delete(2)}"
