90b9c5153787c84c93b29caf1837920247942a3a
Omar Abbasi <omarmabbasi@gmail.com>
2019-05-16 21:16:37 -0700

    Wrote test cases for Class Stack

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -15,3 +15,13 @@
       @stack[-1]
     end
   end
+
+  test_stack = Stack.new
+  puts "Push 1: #{test_stack.push(1)}"
+  puts "Pop: #{test_stack.pop}"
+  puts "Push 1: #{test_stack.push(1)}"
+  puts "Push 2: #{test_stack.push(2)}"
+  puts "Push 3: #{test_stack.push(3)}"
+  puts "Peek: #{test_stack.peek}"
+  puts "Pop: #{test_stack.pop}"
+  puts "Peek: #{test_stack.peek}"
