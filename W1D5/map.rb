1df6c9e086c3a224f846b7597e8ea69601ea486a
Omar Abbasi <omarmabbasi@gmail.com>
2019-05-17 01:24:12 -0700

    Implemented show method to display all key/value pairs

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -29,17 +29,17 @@ class Map
         else
             #debugger
             @map[idx] = nil
-            @map.compact
+            @map.compact!
         end
     end
 
     def show
-
+        @map
     end
 
     def has_key?(to_check, key)
         to_check.each.with_index{|h, idx| return idx if h[0] == key } #return index of key/value pair if key exists, else return false
-        return false
+         false
     end
 
 end
@@ -52,3 +52,7 @@ p "return false if key doesn't exist: #{test_map.get(3)}"
 p "return value if key exists: #{test_map.get(2)}"
 puts "return false if key doesn't exist: #{test_map.delete(5)}"
 puts "delete value if key exists: #{test_map.delete(2)}"
+test_map.set(4,2)
+test_map.set(3,3)
+test_map.set(2,4)
+puts "Fill Hash and Display All Key/Value pairs #{test_map.show}"
