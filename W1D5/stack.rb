5c23478685964e73d605f0665b91f7268fc4d0bc
Omar Abbasi <omarmabbasi@gmail.com>
2019-05-16 21:01:38 -0700

    Class Stack framework

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -1,3 +1,17 @@
-class Stack
+  class Stack
+    def initialize
+      # create ivar to store stack here!
+    end
 
-end
+    def push(el)
+      # adds an element to the stack
+    end
+
+    def pop
+      # removes one element from the stack
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+    end
+  end
