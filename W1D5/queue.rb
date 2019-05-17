360a22c27f6673eacafd95874e0d5fac2d6dac36
Omar Abbasi <omarmabbasi@gmail.com>
2019-05-16 21:25:12 -0700

    Implemented initialize, enqueue, dequeue, and peek methods for Class Queue

--- W1D5/queue.rb
+++ W1D5/queue.rb
@@ -1,17 +1,17 @@
 class Queue
     def initialize
-        
+        @queue = []        
     end
 
     def enqueue(el)
-    
+        queue += [el]    
     end
     
     def dequeue
-    
+        queue.delete_at(0)    
     end
     
     def peek
-    
+        return queue[0]
     end
 end
